b5d382d9f8a68ffd630ed2dfdcdd297a555b2b32
Josine Min <josine.min@bristol.ac.uk>
2018-05-17 10:57:04 +0100

    assoc table

--- input_prep/scripts/combine_assoc_meta_table.sh
+++ input_prep/scripts/combine_assoc_meta_table.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-head -n1 /panfs/panasas01/shared-godmc/database_files/assoc_meta1.csv > /panfs/panasas01/shared-godmc/database_files/assoc_meta_all.csv |
+head -n1 /panfs/panasas01/shared-godmc/database_files/assoc_meta1.csv > /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv &&
 
-tail -n +2 /panfs/panasas01/shared-godmc/database_files/assoc_meta*.csv >>
- for i in {2..}
+tail -n +2 -q /panfs/panasas01/shared-godmc/database_files/assoc_meta*.csv >> /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv
 
+awk -F"," 'BEGIN{OFS=",";} {print $2,$1,$21,$4,$5,$15,$18,$19,$20,$3,$16,$23,$6,$7,$8,$9,$12,$22,$10,$11,$13,$14,$17,$18}' < /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv > /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv2
+mv /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv2 /projects/MRC-IEU/research/data/godmc/_devs/GODMC_Analysis/data/database-files/assoc_meta_all.csv
 
-
-awk -F"," 'BEGIN{OFS=",";} {print $2,$1,$21,$4,$5,$15,$18,$19,$20,$3,$16,$23,$6,$7,$8,$9,$12,$22,$10,$11,$13,$14,$17;$18}' < assoc_meta_all.csv > assoc_meta_all.csv2
-mv assoc_meta_all.csv2 assoc_meta_all.csv
-
+cd ..
---
+++ input_prep/scripts/submit_assoc_meta3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#PBS -N db-assoc
+#PBS -o assoc-output
+#PBS -e assoc-error
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=8
+#PBS -S /bin/bash
+#PBS -t 201-300
+
+
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+
+set -e
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  PBS_ARRAYID=${1}
+fi
+
+i=${PBS_ARRAYID}
+
+cd /panfs/panasas01/sscm/epzjlm/repo/godmc-database/input_prep/scripts
+R CMD BATCH --no-save --no-restore '--args '$i'' make_assoc_meta_table.R assoc$i.Rout
