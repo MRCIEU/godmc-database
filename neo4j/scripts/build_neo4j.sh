da8939037e6b7d149a1d75056a027d5172df9506
Gibran Hemani <explodecomputer@gmail.com>
2018-08-17 17:13:29 +0100

    updated

--- neo4j/scripts/build_neo4j.sh
+++ neo4j/scripts/build_neo4j.sh
@@ -8,6 +8,7 @@ rm -rf ../neo4j-community-3.4.6/data/databases/godmc.db
 --ignore-missing-nodes=true \
 --nodes:cpg "../data/mqtl/cpgs_header.csv,../data/mqtl/cpgs.csv.gz" \
 --nodes:snp "../data/mqtl/snps_header.csv,../data/mqtl/snps.csv.gz" \
+--nodes:gene "../data/mqtl/gencode_header.csv,../data/mqtl/gencode.csv.gz" \
 --nodes:trait "../data/trait-cpg/trait_header.csv,../data/trait-cpg/trait.csv.gz" \
 --nodes:lola "../data/2d/lola_header.csv,../data/2d/lola.csv.gz" \
 --relationships:anno "../data/2d/snp_header.csv,../data/2d/snp.csv.gz" \
@@ -21,5 +22,7 @@ rm -rf ../neo4j-community-3.4.6/data/databases/godmc.db
 --relationships:mr "../data/cpg-trait/full_header.csv,../data/cpg-trait/full.csv.gz" \
 --relationships:mr:coloc "../data/cpg-trait/coloc_header.csv,../data/trait-cpg/coloc.csv.gz" \
 --relationships:ga "../data/trait-cpg/inst_header.csv,../data/trait-cpg/inst.csv.gz" \
---relationships:ga:mqtl "../data/mqtl/mqtl_header.csv,../data/mqtl/mqtl.csv.gz"
+--relationships:ga:mqtl "../data/mqtl/mqtl_header.csv,../data/mqtl/mqtl.csv.gz" \
+--relationships:anno "../data/mqtl/snp_gene_header.csv,../data/mqtl/snp_gene.csv.gz" \
+--relationships:anno "../data/mqtl/cpg_gene_header.csv,../data/mqtl/cpg_gene.csv.gz"
 
--- neo4j/scripts/mqtl.r
+++ neo4j/scripts/mqtl.r
@@ -1,11 +1,13 @@
 library(dplyr)
+library(GenomicRanges)
 source("utils.r")
 
 cpgdat <- "../data/mqtl/cpgs.rdata"
 graphdat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/05_cis-trans-networks/results/graph.rdata"
 snpdat <- "../data/mqtl/snps.rdata"
 mqtldat <- "../data/mqtl/assoc_meta_all.csv"
-outdir <- "../data/mqtl"
+genedat <- "../data/mqtl/genes.csv"
+
 
 ##
 
@@ -29,6 +31,41 @@ names(out_df2) <- modify_node_headers_for_neo4j(out_df2, "id", "snp")
 write_out(out_df2, "../data/mqtl/snps", header=TRUE)
 
 
+##
+
+
+gen <- read.csv(genedat, stringsAsFactors=FALSE)
+gen <- subset(gen, !duplicated(name))
+
+# gen$chr <- as.character(gen$chr)
+# gen$chr[gen$chr == "23"] <- "X"
+# gen$chr[gen$chr == "24"] <- "Y"
+# gen$chr[gen$chr == "25"] <- "MT"
+# gen$chr <- paste0("chr", gen$chr)
+
+df <- subset(df, !is.na(df$`pos:INT`))
+df$chr <- gsub("chr", "", df$chr)
+df$chr[df$chr == "X"] <- 23
+df$chr[df$chr == "Y"] <- 24
+df$chr <- as.numeric(df$chr)
+
+gr <- GRanges(gen$chr, IRanges(start=gen$start_pos - 25000, end=gen$start_pos + 25000), name=gen$name)
+cpg <- GRanges(df$chr, IRanges(start=df$`pos:INT`, end=df$`pos:INT`), cpg=df$name)
+snp <- GRanges(out_df2$chr, IRanges(start=out_df2$`pos:INT`, end=out_df2$`pos:INT`), snp=out_df2$name)
+
+cpgi <- findOverlaps(gr, cpg) %>% as_data_frame
+snpi <- findOverlaps(gr, snp) %>% as_data_frame
+
+cpgi <- data_frame(cpg=df$name[cpgi$subjectHits], gene=gen$name[cpgi$queryHits])
+snpi <- data_frame(snp=out_df2$name[snpi$subjectHits], gene=gen$name[snpi$queryHits])
+
+names(gen) <- modify_node_headers_for_neo4j(gen, "name", "gene")
+names(cpgi) <- modify_rel_headers_for_neo4j(cpgi, "gene", "gene", "cpg", "cpg")
+names(snpi) <- modify_rel_headers_for_neo4j(snpi, "gene", "gene", "snp", "snp")
+
+write_out(gen, "../data/mqtl/gencode", header=TRUE)
+write_out(cpgi, "../data/mqtl/cpg_gene", header=TRUE)
+write_out(snpi, "../data/mqtl/snp_gene", header=TRUE)
 
 ## 
 
@@ -36,5 +73,5 @@ write_out(out_df2, "../data/mqtl/snps", header=TRUE)
 mqtl <- read.csv(mqtldat, nrows=5000)
 names(mqtl) <- modify_rel_headers_for_neo4j(mqtl, "snp", "snp", "cpg", "cpg")
 
-write.table(mqtl[0,], file=paste0(outdir, "/mqtl_header.csv"), row.names=FALSE, col.names=TRUE, sep=",")
+write.table(mqtl[0,], file="../data/mqtl//mqtl_header.csv", row.names=FALSE, col.names=TRUE, sep=",")
 
