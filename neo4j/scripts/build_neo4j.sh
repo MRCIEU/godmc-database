19111cc2384b27897c8f203102e404e273f78ad4
Gibran Hemani <gh13047@shark.epi.bris.ac.uk>
2018-08-17 18:29:19 +0100

    updated

--- neo4j/scripts/build_neo4j.sh
+++ neo4j/scripts/build_neo4j.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-version="3.4.5"
+version=`cat version.txt`
 
-wget http://dist.neo4j.org/neo4j-community-${version}-unix.tar.gz
-tar xzvf neo4j-community-${version}-unix.tar.gz
 
-cd ../
+rm -rf ../neo4j-community-${version}/data/databases/godmc.db
 
-rsync -avu --progress data/ gh13047@shark.epi.bris.ac.uk:godmc-database/neo4j/data/
-
-rm -rf neo4j-community-${version}/data/databases/godmc.db
-rsync -avu --progress neo4j-community-${version}/ gh13047@shark.epi.bris.ac.uk:godmc-database/neo4j/neo4j-community-${version}/ 
-
-neo4j-community-${version}/bin/neo4j-admin import \
+../neo4j-community-${version}/bin/neo4j-admin import \
 --database godmc.db \
 --id-type string \
 --ignore-missing-nodes=true \
---
+++ neo4j/scripts/rsync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=`cat version.txt`
+
+
+cd ../
+
+wget http://dist.neo4j.org/neo4j-community-${version}-unix.tar.gz
+tar xzvf neo4j-community-${version}-unix.tar.gz
+
+rsync -avu --progress data/ gh13047@shark.epi.bris.ac.uk:godmc-database/neo4j/data/
+
+rm -rf neo4j-community-${version}/data/databases/godmc.db
+rsync -avu --progress neo4j-community-${version}/ gh13047@shark.epi.bris.ac.uk:godmc-database/neo4j/neo4j-community-${version}/
+
+
