52d1767e94caaf1fc21662591f012ad16d6224a2
Gibran Hemani <explodecomputer@gmail.com>
2018-08-17 14:31:00 +0100

    added 2d

---
+++ neo4j/scripts/2d.r
@@ -0,0 +1,31 @@
+library(dplyr)
+source("utils.r")
+
+annodat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/11_2d-enrichments/data/annotations.rdata"
+
+difresdat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/11_2d-enrichments/results/difres/difres0.rdata"
+
+load(annodat)
+load(difresdat)
+
+anno$id_lola <- paste0("lola", 1:nrow(anno))
+snpres$anno <- paste0("lola", snpres$anno)
+cpgres$anno <- paste0("lola", cpgres$anno)
+
+glimpse(difres)
+difres$Var1 <- paste0("lola", difres$Var1)
+difres$Var2 <- paste0("lola", difres$Var2)
+names(difres) <- c("Var1", "Var2", "min", "q25", "median", "mean", "q75", "max", "val", "dif", "sds", "sddif")
+
+names(anno) <- modify_node_headers_for_neo4j(anno, "id_lola", "lola")
+write_out(anno, "../data/2d/lola", header=TRUE)
+
+names(snpres) <- modify_rel_headers_for_neo4j(snpres, "anno", "lola", "snp", "snp")
+write_out(snpres, "../data/2d/snp", header=TRUE)
+
+names(cpgres) <- modify_rel_headers_for_neo4j(cpgres, "anno", "lola", "cpg", "cpg")
+write_out(cpgres, "../data/2d/cpg", header=TRUE)
+
+names(difres) <- modify_rel_headers_for_neo4j(difres, "Var1", "lola", "Var2", "lola")
+write_out(difres, "../data/2d/2d", header=TRUE)
+
