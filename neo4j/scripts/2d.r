224dd895346e5a67a7fa15bbf6b05c1ba8ef7c3d
Gibran Hemani <explodecomputer@gmail.com>
2018-08-18 12:54:11 +0100

    upd

--- neo4j/scripts/2d.r
+++ neo4j/scripts/2d.r
@@ -2,13 +2,18 @@ library(dplyr)
 source("utils.r")
 
 annodat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/11_2d-enrichments/data/annotations.rdata"
+annobdat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/11_2d-enrichments/data/blood.rdata"
 
 difresdat <- "/mnt/storage/private/mrcieu/research/GODMC_Analysis/godmc_phase2_analysis/11_2d-enrichments/results/difres/difres0.rdata"
 
 load(annodat)
+a <- anno
+load(annobdat)
 load(difresdat)
 
-anno$id_lola <- paste0("lola", 1:nrow(anno))
+anno$index <- paste0("lola", 1:nrow(anno))
+names(anno)[names(anno) == "antibody2"] <- "name"
+anno <- anno[,ncol(anno):1]
 snpres$anno <- paste0("lola", snpres$anno)
 cpgres$anno <- paste0("lola", cpgres$anno)
 
@@ -17,7 +22,7 @@ difres$Var1 <- paste0("lola", difres$Var1)
 difres$Var2 <- paste0("lola", difres$Var2)
 names(difres) <- c("Var1", "Var2", "min", "q25", "median", "mean", "q75", "max", "val", "dif", "sds", "sddif")
 
-names(anno) <- modify_node_headers_for_neo4j(anno, "id_lola", "lola")
+names(anno) <- modify_node_headers_for_neo4j(anno, "index", "lola")
 write_out(anno, "../data/2d/lola", header=TRUE)
 
 names(snpres) <- modify_rel_headers_for_neo4j(snpres, "anno", "lola", "snp", "snp")
