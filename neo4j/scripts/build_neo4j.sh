27ea15580eb518e2c684edb24bc73375262498be
Gibran Hemani <explodecomputer@gmail.com>
2018-08-20 02:02:49 +0100

    added suptab

--- neo4j/scripts/build_neo4j.sh
+++ neo4j/scripts/build_neo4j.sh
@@ -2,10 +2,9 @@
 
 version=`cat version.txt`
 
-cd ../
-rm -rf neo4j-community-${version}/data/databases/godmc.db
+rm -rf ../neo4j-community-${version}/data/databases/godmc.db
 
-neo4j-community-${version}/bin/neo4j-admin import \
+../neo4j-community-${version}/bin/neo4j-admin import \
 --database godmc.db \
 --id-type string \
 --ignore-missing-nodes=true \
@@ -16,16 +15,16 @@ neo4j-community-${version}/bin/neo4j-admin import \
 --nodes:lola "../data/2d/lola_header.csv,../data/2d/lola.csv.gz" \
 --relationships:anno "../data/2d/snp_header.csv,../data/2d/snp.csv.gz" \
 --relationships:anno "../data/2d/cpg_header.csv,../data/2d/cpg.csv.gz" \
---relationships:2d "../data/2d/2d_header.csv,../data/2d/2d.csv.gz" \
---relationships:mr:coloc "../data/communities/coloc_header.csv,../data/communities/coloc.csv.gz" \
+--relationships:twod "../data/2d/2d_header.csv,../data/2d/2d.csv.gz" \
+--relationships:coloc "../data/communities/coloc_header.csv,../data/communities/coloc.csv.gz" \
 --relationships:mr "../data/trait-cpg/res_header.csv,../data/trait-cpg/res.csv.gz" \
 --relationships:mr "../data/trait-cpg/het_header.csv,../data/trait-cpg/het.csv.gz" \
 --relationships:mr "../data/trait-cpg/plei_header.csv,../data/trait-cpg/plei.csv.gz" \
 --relationships:mr "../data/trait-cpg/full_header.csv,../data/trait-cpg/full.csv.gz" \
 --relationships:mr "../data/cpg-trait/full_header.csv,../data/cpg-trait/full.csv.gz" \
---relationships:mr:coloc "../data/cpg-trait/coloc_header.csv,../data/trait-cpg/coloc.csv.gz" \
+--relationships:coloc "../data/cpg-trait/coloc_header.csv,../data/cpg-trait/coloc.csv.gz" \
 --relationships:ga "../data/trait-cpg/inst_header.csv,../data/trait-cpg/inst.csv.gz" \
---relationships:ga:mqtl "../data/mqtl/mqtl_header.csv,../data/mqtl/mqtl.csv.gz" \
+--relationships:ga "../data/mqtl/mqtl_header.csv,../data/mqtl/mqtl.csv.gz" \
 --relationships:anno "../data/mqtl/snp_gene_header.csv,../data/mqtl/snp_gene.csv.gz" \
 --relationships:anno "../data/mqtl/cpg_gene_header.csv,../data/mqtl/cpg_gene.csv.gz"
 
---
+++ neo4j/scripts/mr_suptab.r
@@ -0,0 +1,28 @@
+library(dplyr)
+library(data.table)
+a <- fread("zcat ../data/trait-cpg/res.csv.gz")
+b <- scan("../data/trait-cpg/res_header.csv", what="character", sep=",")
+names(a) <- c("trait", "cpg", "method", "nsnp", "beta", "se", "pval", "chunk")
+a <- subset(a, select=-c(chunk))
+load("../data/trait_id_master.rdata")
+temp <- subset(master, select=c(name_10, id_06))
+index <- match(a$trait, master$id_06)
+a$trait <- master$name_10[index]
+
+
+
+b <- fread("zcat ../data/cpg-trait/coloc.csv.gz")
+names(b) <- c("sentinel_snp", "cpg", "nsnp", "H0", "H1", "H2", "H3", "H4", "effect_allele", "other_allele", "effect_allele_freq", "waldratio_beta", "waldratio_se", "waldratio_pval", "trait_samplesize", "trait")
+
+index <- match(b$trait, master$id_06)
+b$trait <- master$name_10[index]
+
+setcolorder(b, c("cpg", "trait", "nsnp", "H0", "H1", "H2", "H3", "H4", "sentinel_snp", "effect_allele", "other_allele", "effect_allele_freq", "waldratio_beta", "waldratio_se", "waldratio_pval", "trait_samplesize"))
+
+
+b <- subset(b, H4 > 0.5) %>% arrange(desc(H4))
+
+write.csv(a, file="../data/trait-cpg.csv")
+write.csv(b, file="../data/cpg-trait.csv")
+
+
